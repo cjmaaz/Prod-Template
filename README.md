7f48f3a00bd8bde21e8321f4d06ebda22f1a6c6e
Maaz Rahman <maaz@techie.com>
2025-05-06 00:34:09 +0530

    prettier config

---
+++ README.md
@@ -0,0 +1,3 @@
+### Dev Initialization
+```bash
+```
---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  semi: true,             // Always end lines with a semicolon
+  singleQuote: true,      // Use single quotes everywhere
+  trailingComma: 'all',   // Enable trailing commas
+  printWidth: 100,        // Wrap lines at 100 characters
+  tabWidth: 2,            // Use 2 spaces per tab
+  useTabs: false,         // Never use tabs
+  arrowParens: 'always'   // Always include parens. Example: (x) => x
+};
