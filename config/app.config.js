3cf2c35324a721c6fad9d6784af023cfaa412ab9
Maaz Rahman <maaz@techie.com>
2025-05-07 14:54:24 +0530

    feat: :tada: folder structure setup

---
+++ config/app.config.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env' });
+
+const environment = process.env.NODE_ENV || 'development';
+
+export default {
+  // eslint-disable-next-line no-magic-numbers
+  port: process.env.PORT || 3000,
+  apiPrefix: environment === 'production' ? '/api' : '/dev-api',
+};
